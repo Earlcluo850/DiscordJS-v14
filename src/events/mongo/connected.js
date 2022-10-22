fe63c47909bb17adee696474ff437c35d4c7836a
Earlcluo850 <earlcluo@gmail.com>
2022-10-22 12:12:34 +0700

    commit

--- src/events/mongo/connected.js
+++ src/events/mongo/connected.js
@@ -3,6 +3,6 @@ const chalk = require("chalk");
 module.exports = {
   name: "connected",
   execute() {
-    console.log(chalk.green("[MONGODB]"), "Connected!");
+    console.log(chalk.green("[MONGO]"), "Connected!");
   },
 };
